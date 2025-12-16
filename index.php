cc2662c2db9a81ed23308b5b27ffbcda0f28ad15
VirtusStack <payalgupta6403@gmail.com>
2025-12-16 19:22:37 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'templates/include/header.php'; ?>
+
+<!-- Home Page Content -->
+<section class="hero text-center py-5 bg-light">
+  <div class="container">
+    <h1>Welcome to Aurika</h1>
+    <p class="lead">Professional & modern web solutions</p>
+     </div>
+</section>
+
+<?php include 'templates/include/footer.php'; ?>
