21ede3b3e957f271f4616e41b5cbcf978c375de8
VirtusStack <payalgupta6403@gmail.com>
2025-12-19 14:44:32 +0530

    Update programs.php

--- programs.php
+++ programs.php
@@ -1,5 +1,9 @@
 <?php
 require_once __DIR__ . '/config/config.php';
+
+// Set current page for active nav
+$currentPage = 'programs.php';
+
 include __DIR__ . '/templates/include/header.php';
 ?>
 <!-- PROGRAMS : AURIKA KNOWLEDGE FOUNDATION -->
