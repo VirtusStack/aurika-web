ce8ed187e143b45439a8f4f4fcbce711715d586e
VirtusStack <payalgupta6403@gmail.com>
2025-12-19 13:32:32 +0530

    Update header.php

--- templates/include/header.php
+++ templates/include/header.php
@@ -14,7 +14,8 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
 
 <!-- Custom CSS -->
-<link rel="stylesheet" href="/aurika-web/assets/css/style.css">
+<link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css">
+
 </head>
 <body>
 
