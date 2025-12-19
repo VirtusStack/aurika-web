a35bdefdaa3b51a56c06b3afa633a6fa891ee16d
VirtusStack <payalgupta6403@gmail.com>
2025-12-19 13:32:15 +0530

    Update programs.php

--- programs.php
+++ programs.php
@@ -1,5 +1,7 @@
-<?php include __DIR__ . '/templates/include/header.php'; ?>
-
+<?php
+require_once __DIR__ . '/config/config.php';
+include __DIR__ . '/templates/include/header.php';
+?>
 <!-- PROGRAMS : AURIKA KNOWLEDGE FOUNDATION -->
 <!-- PAGE HEADER -->
 <section class="py-5 bg-light">
